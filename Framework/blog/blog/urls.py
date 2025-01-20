da81a7599beb0287698268babcf54ed670710fcb
username <kholidwirautama1@gmail.com>
2025-01-20 12:15:02 +0700

    + Register

--- Framework/blog/blog/urls.py
+++ Framework/blog/blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import UserRegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
